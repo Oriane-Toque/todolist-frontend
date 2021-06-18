1b5871f04c6050503660c80fbf948625d6dd5cdd
Oriane-Toque <oriane.toque@gmail.com>
2021-06-18 10:53:02 +0200

    error message & delete error message methods: done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,44 @@
 const app = {
 
-  init: function(){
+  init: function () {
     console.log('app.init executé');
     tasksList.init();
     newTaskForm.init();
     categoriesList.init();
+  },
+
+  /**
+   * Méthode qui génère et affiche un message d'erreur
+   * 
+   * @param {*} parentDOM 
+   * @param {*} message 
+   */
+  addErrorMessage: function (parentDOM, message) {
+    // si aucun message d'erreur existe alors création d'un message
+    // sinon rien
+    if (!document.querySelector('form > p')) {
+      const errorContent = document.createElement('p');
+      const errorMessage = message;
+
+      errorContent.textContent = errorMessage;
+
+      parentDOM.prepend(errorContent);
+    }
+  },
+
+  /**
+   * Méthode qui supprime un message d'erreur
+   * 
+   * @param {*} form 
+   * @param {*} locationMessage 
+   */
+  deleteErrorMessage: function (form, locationMessage) {
+    if (locationMessage) {
+
+      form.removeChild(locationMessage);
+    }
   }
-  
+
 };
 
 // On ne va pas lancer init "en dur"
@@ -16,5 +48,3 @@ const app = {
 //! la fonction ou methode a executer lorsque l'event est capté ne dois
 //! JAMAIS avoir de parentheses ! 
 document.addEventListener('DOMContentLoaded', app.init);
-
-
--- assets/js/components/newTaskForm.js
+++ assets/js/components/newTaskForm.js
@@ -2,14 +2,14 @@ const newTaskForm = {
 
 
   // initialisation du composant
-  init: function(){
+  init: function () {
     newTaskForm.bindNewTaskFormEventListener();
   },
 
   /**
    * Méthode pour l'ajout d'un  eventlistener liés au formulaire d'ajout d'une tâche
    */
-  bindNewTaskFormEventListener: function(){
+  bindNewTaskFormEventListener: function () {
     // on récupère le formulaire d'ajout d'un" tache (le form qui est dans un element de classe task--add)
     const newTaskFormElement = document.querySelector('.task--add form');
 
@@ -22,7 +22,7 @@ const newTaskForm = {
    * 
    * @param {*} evt 
    */
-  handleNewTaskFormSubmit: function(evt){
+  handleNewTaskFormSubmit: function (evt) {
     // on empeche la page de se recharger :
     evt.preventDefault();
     // recupération du form
@@ -39,15 +39,48 @@ const newTaskForm = {
     // clear formulaire
     newTaskFormElement.reset();
 
-    // ci dessous je met le focus sur l'input de la tache
+    // ci dessous je remet le focus après soumission du formulaire
     taskTitleFieldElement.focus();
-    
-    // j'imagine une methode qui va nous permettre de créer une nouvelle tache
-    // cette methode va recevoir 2 arguments : le nom de la tache et le nom de la categorie
-    const newTaskElement = task.createTaskElement(newTaskTitle, newTaskCategoryName);
-    // j'imagine une methode dont le but sera de nous afficher la tache ( l'inserer dans la lsite des taches);
-    tasksList.insertTaskIntoTasksList(newTaskElement);
 
-  }
+    // si le formulaire a été dumement remplis
+    if (newTaskForm.checkAddNewTask(newTaskTitle) === true) {
+
+      // si présence d'un message d'erreur : suppression du message
+      const errorAddTask = document.querySelector('form > p');
+      app.deleteErrorMessage(newTaskFormElement, errorAddTask);
+
+      // j'imagine une methode qui va nous permettre de créer une nouvelle tache
+      // cette methode va recevoir 2 arguments : le nom de la tache et le nom de la categorie
+      const newTaskElement = task.createTaskElement(newTaskTitle, newTaskCategoryName);
+      // j'imagine une methode dont le but sera de nous afficher la tache ( l'inserer dans la lsite des taches);
+      tasksList.insertTaskIntoTasksList(newTaskElement);
+    }
+    // sinon message d'erreur
+    else {
+
+      const errorMessage = "Veuillez indiquer le titre de la tâche";
+      // génère et affiche un message d'erreur
+      app.addErrorMessage(newTaskFormElement, errorMessage);
+
+    }
+
+  },
+
+  /**
+   * Méthode qui vérifie si le titre de la tâche a été indiqué
+   * 
+   * @param {*} newTaskTitle 
+   * @return {bool} 
+   */
+  checkAddNewTask: function (newTaskTitle) {
+
+    if (newTaskTitle === "") {
+      console.log(false);
+      return false;
+    } else {
+      console.log(true);
+      return true;
+    }
+  },
 
 }
